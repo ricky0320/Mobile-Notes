065b182375b3cb4da5d0180f43242e8fd9729396
Ricky Leung <ricxx0320@gmail.com>
2022-02-24 02:19:46 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Note Taker Starter Code
+# Mobile Notes
+Mobile Notes is a note taker application that let users save and delete quick notes. User is able to input note title and description on the right side of the page and save their notes by pressing the save icon near the right top corner. Saved notes are appended on the left side and users are able to delete them as well. Don't hesitate to try it out!
+
+#### This page is licensed under (click badge for license page): 
+[![license](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+* Bootstrap 
+* Express.js
+* Heroku
+* Npm Packages - fs, path, express
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license) 
+* [Contributing](#contributing) 
+* [Contact](#contact)
+
+## Installation
+`git clone https://github.com/kcheykim/note-taker.git`<br />
+`npm init -y`<br />
+`npm i express`<br />
+
+## Usage
+Link to Application: <br />
+https:
+![Screenshot](./public/assets/images/note-taker1.jpg)<br/>
+![Screenshot](./public/assets/images/note-taker2.jpg)
+
+## Contributors
+Ricky Leung
+
+## License
+Notice: This license is covered under (https://opensource.org/licenses/MIT)
+
+## Contributing
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
+
+## Contact  
+GitHub: http://github.com/ricky0320
+Email: ricxx0320@gmail.com
--- server.js
+++ server.js
@@ -6,75 +6,72 @@ const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// set to public, parse JSON
+app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
 
-app.use(express.static('public'));
-
-app.use(express.urlencoded({ extended: true }));
-// parse JSON
-app.use(express.json());
-
-
-app.get('/app/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './db/db,json'));
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add notes`);
-  const { title, text } = req.body;
-  if (title && text) {
-      fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
-          if (err) { console.log(err) } else {
-              const savedNotes = JSON.parse(data);
-              savedNotes.push({ title, text, id: uuid() });
-              // Write the string to a file
-              fs.writeFile(`./db/db.json`, JSON.stringify(savedNotes), (err) => err ?
-                  console.error(err) : console.log(`${title} is written to JSON file`)
-              );
-          }
-      })
-      res.json({ status: 'SUCCESS', body: { title, text, id: uuid() } });
-  } else { res.json('Error: Cannot add note'); }
+    console.info(`${req.method} request received to add notes`);
+    const { title, text } = req.body;
+    if (title && text) {
+        fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+            if (err) { console.log(err) } else {
+                const savedNotes = JSON.parse(data);
+                savedNotes.push({ title, text, id: uuid() });
+                // Write the string to a file
+                fs.writeFile(`./db/db.json`, JSON.stringify(savedNotes), (err) => err ?
+                    console.error(err) : console.log(`${title} is written to JSON file`)
+                );
+            }
+        })
+        res.json({ status: 'SUCCESS', body: { title, text, id: uuid() } });
+    } else { res.json('Error: Cannot add note'); }
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
-      if (err) { console.log(err); }
-      let savedNotes = JSON.parse(data);
-      for (let i = 0; i < savedNotes.length; i++) {
-          if (savedNotes[i].id === req.params.id) { savedNotes.splice(i, 1); }
-      }
-      fs.writeFile(`./db/db.json`, JSON.stringify(savedNotes), (err) => err ?
-          console.error(err) : console.log(`Note deleted ID: ${req.params.id}`)
-      );
-      res.send(savedNotes);
-  })
+    fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+        if (err) { console.log(err); }
+        let savedNotes = JSON.parse(data);
+        for (let i = 0; i < savedNotes.length; i++) {
+            if (savedNotes[i].id === req.params.id) { savedNotes.splice(i, 1); }
+        }
+        fs.writeFile(`./db/db.json`, JSON.stringify(savedNotes), (err) => err ?
+            console.error(err) : console.log(`Note deleted ID: ${req.params.id}`)
+        );
+        res.send(savedNotes);
+    })
 });
 
 app.put('/api/notes/:id', (req, res) => {
-  console.log(JSON.parse(req.params.id));
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) { console.log(err) }
-      let savedNotes = JSON.parse(data);
-      let select = savedNotes.find((element) => element.id === req.params.id);
-      if (select) {
-          let update = { title: req.body.title, text: req.body.text, id: select.id };
-          savedNotes.splice(savedNotes.indexOf(select), 1, update);
-          fs.writeFile('./db/db.json', JSON.stringify(savedNotes), (err) => err ?
-              console.error(err) : console.log(`Note update ID: ${savedNotes.id}`)
-          );
-          res.json(savedNotes);
-      }
-  })
+    console.log(JSON.parse(req.params.id));
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) { console.log(err) }
+        let savedNotes = JSON.parse(data);
+        let select = savedNotes.find((element) => element.id === req.params.id);
+        if (select) {
+            let update = { title: req.body.title, text: req.body.text, id: select.id };
+            savedNotes.splice(savedNotes.indexOf(select), 1, update);
+            fs.writeFile('./db/db.json', JSON.stringify(savedNotes), (err) => err ?
+                console.error(err) : console.log(`Note update ID: ${savedNotes.id}`)
+            );
+            res.json(savedNotes);
+        }
+    })
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
